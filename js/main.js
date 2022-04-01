f8c4aeac238e3d427a681c2e55f9bec4ea48eaaf
song22ee <ann7335@naver.com>
2022-04-01 16:26:12 +0900

    Create css,js,img

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", ()=>{
+    const grid = new Isotope(".product_section", {
+        itemSelector: "article",
+        columnWidth:"article",
+        transitionDuration: "0.5s"
+    });
+});
+
+
