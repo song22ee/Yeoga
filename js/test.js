6bebdcecf24f339d79d8d1e2f33263a140fa1e2e
song22ee <ann7335@naver.com>
2022-04-08 21:37:18 +0900

    iframe 첫 이용

---
+++ js/test.js
@@ -0,0 +1,26 @@
+//iframe의 src 바꾸기
+function changeIframeUrl(url) {
+    console.log('do');
+    document.getElementById('iframe__container').src = url;
+}
+
+function readImage(input) {
+    // 인풋 태그에 파일이 있는 경우
+    if (input.files && input.files[0]) {
+        // 이미지 파일인지 검사 (생략)
+        // FileReader 인스턴스 생성
+        const reader = new FileReader();
+        // 이미지가 로드가 된 경우
+        reader.onload = e => {
+            const previewImage = document.getElementById('account-form__preview-img');
+            previewImage.src = e.target.result;
+        };
+        // reader가 이미지 읽도록 하기
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+// input file에 change 이벤트 부여
+const inputImage = document.getElementById('account-form__input-img');
+inputImage.addEventListener('change', e => {
+    readImage(e.target);
+});
